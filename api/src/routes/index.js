5123340b4c61b4efb4ad51edb93abd968decabdd
Juan Saravia <saraviamartin16@hotmail.com>
2023-04-16 15:08:51 -0300

    refactorizando

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,17 +9,11 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get("/countries", getAllCountries);
-  
-router.get("/countries/:id", getCountriById);
-  
-  router.get("/countries/name?=", (req, res) => {
-    res.status(200).send("Esta ruta debe obtener todos aquellos países que coinciden con el nombre recibido por query. (No es necesario que sea una coincidencia exacta).")
-  });
-  
-  router.post('/activity', getNewActivity);
-  router.get('/activity/', getAllActivity);
-  router.get('/activity/:id', getActId);
+router.get("/countries", getAllCountries); 
+router.get("/countries/:id", getCountriById); 
+router.post('/activity', getNewActivity);
+router.get('/activity/', getAllActivity);
+router.get('/activity/:id', getActId);
 
 
 module.exports = router;
