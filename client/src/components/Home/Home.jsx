134e64614077e88a1aabc42811118df17b57dde5
Juan Saravia <saraviamartin16@hotmail.com>
2023-04-24 18:10:01 -0300

    pagination, detail, errors

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,7 +3,11 @@ import Cards from "../Cards/Cards"
 import Navbar from "../Navbar/Navbar";
 import { useEffect } from "react";
 import {useDispatch, useSelector} from "react-redux"
+import { useState } from "react";
 import { getAllCountries }from "../../redux/Actions/actions"
+import Pagination from "../Pagination/Pagination"
+import Detail from "../Detail/Detail"
+
 // import SortByName from "../sorts/SortByName";
 // import SortByPopulation from "../sorts/SortByPopulation";
 
@@ -11,7 +15,9 @@ import { getAllCountries }from "../../redux/Actions/actions"
 
 function Home(){
 
-//   const [order, setOrder] = useState("");
+  const [order, setOrder] = useState("");
+  const [show, setShow] = useState(false);
+
 
 const dispatch = useDispatch();
 const allCountries = useSelector((state) => state.allCountries);
@@ -20,17 +26,61 @@ useEffect(() => {
 }, [dispatch]);
 
 
-console.log(allCountries)
+// console.log(allCountries)
 //   let handleReload = (e) => {
 //     e.preventDefault();
 //     dispatch(getAllCountries());
 //   };
+const [currentPage, setCurrentPage] = useState(1);
+  const countriesPerPage = 12;
+  let indFirstCountry = 0;
+  let indLastCountry = 0;
+
+  if (currentPage === 1) {
+    indFirstCountry = 0;
+    indLastCountry = 0;
+  } else {
+    indFirstCountry = currentPage - 2;
+    indLastCountry = currentPage - 1;
+  }
+  const indexOfLastCountry = currentPage * countriesPerPage;
+  const indexOfFirstCountry = indexOfLastCountry - countriesPerPage;
+
+  const currentCountries =
+    !allCountries.length > 0
+      ? []
+      : allCountries.slice(
+          indexOfFirstCountry + indFirstCountry,
+          indexOfLastCountry + indLastCountry
+        );
+  console.log("array current countries", currentCountries);
+
+  const paged = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  const [loading, setLoading] = useState(false);
+  console.log("show ", show);
+  useEffect(() => {
+    setShow((show) => !show);
+    if (loading) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    }
+  }, [loading]);
 
     return(
       
         <div className="home-container">
           <Navbar/>
          <Cards allCountries={allCountries}/>
+         <Pagination
+         countriesPerPage={countriesPerPage}
+         allCountries={allCountries}
+         paged={paged}
+         key={"page" + currentPage}
+         currentPage={currentPage}
+       />
         </div>
     )
 };
--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,5 +1,40 @@
 import React from "react";
 
-export default function Pagination(){
-    
+ function Pagination({ countriesPerPage, allCountries, paged, currentPage }){
+    let pageNumbers = [];
+  if (Array.isArray(allCountries)) {
+    for (
+      let i = 1;
+      i <= Math.ceil((allCountries.length - 9) / (countriesPerPage + 1)) + 1;
+      i++
+    ) {
+      pageNumbers = [...pageNumbers, i];
+    }
+  } else {
+    pageNumbers = [1];
+  }
+  if (pageNumbers.length > 1) {
+    return (
+      <div>
+        {pageNumbers &&
+          pageNumbers.map((num) => (
+            <button
+              key={"paged" + num}
+              onClick={() => {
+                paged(num);
+              }}
+              value={num}
+              className={currentPage}
+            >
+              {" "}
+              {num}{" "}
+            </button>
+          ))}
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }
+
+export default Pagination;
