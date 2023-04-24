6746f1d3879a62c582845296f1c5a13dda010748
Juan Saravia <saraviamartin16@hotmail.com>
2023-04-24 14:47:01 -0300

    paginado y navbar, error

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,36 @@
+import { Link } from "react-router-dom";
+import {getCountry }from "../../redux/Actions/actions";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 import "./navbar.styles.css";
 
 function Navbar(){
+    const dispatch = useDispatch();
+  const [name, setName] = useState("");
+
+  function handleInputChange(e) {
+    e.preventDefault();
+    setName(e.target.value);
+    }
+  const handleClick = (event) => {
+    event.preventDefault();
+    dispatch(getCountry(name));
+    setName("");
+    }
     return(
         <div className="search-box">
             <h3>Este es el componente Navbar de CoutryApp</h3>
-            <form>
-                <input placeholder="Busqueda"/>
-                <button>Buscar</button>
+            <form onSubmit={(event) => handleClick(event)}>
+                <input placeholder="Search Country"  onChange={(e) => handleInputChange(e)}/>
+                <button onClick={(event) => {
+            handleClick(event);
+          }}>Buscar</button>
             </form>
+        <div>
+             <Link  to='/Activity'>Create Activity</Link>
+             <br />
+          <Link to='/Activities'>Activities List</Link>
+        </div>
         </div>
     )
 };
